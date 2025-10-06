bdc65ec1e5ea1debfa2448afa803692156b14cc2
Amaa2001 <anamaato@gmail.com>
2025-10-06 11:35:05 +0200

    bmi channges

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ let bmi = (weight / (height * height)).toFixed(2);
 let category = "";
 
 if (bmi < 18.5) {
-    category = "Underweight";
+    category = "Undervikt";
 } else if (bmi >= 18.5 && bmi < 24.9) {
-    category = "Normal weight";
+    category = "Normalvikt";
 } else if (bmi >= 25 && bmi < 29.9) {
-    category = "Overweight";
+    category = "Övervikt";
 } else {
-    category = "Obese";
+    category = "Fetma";
 }
 
 document.getElementById("result").innerHTML = 
- `Your BMI is <strong>${bmi}</strong> (${category})`;
+ `Ditt BMI är <strong>${bmi}</strong> (${category})`;
 }
